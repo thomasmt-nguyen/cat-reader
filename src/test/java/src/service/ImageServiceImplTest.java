8b4458df1474f30dcd3e7a0a43848e0599a5920e
nguyen, thomas <thomas_nguyen@intuit.com>
2019-12-28 20:30:31 -0800

    fixed unit test

--- src/test/java/src/service/ImageServiceImplTest.java
+++ src/test/java/src/service/ImageServiceImplTest.java
@@ -1,9 +1,5 @@
 package src.service;
 
-import org.junit.runner.RunWith;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-@RunWith(SpringJUnit4ClassRunner.class)
 public class ImageServiceImplTest {
 
 }
