255d2b61b123f647b105d652b4793de669798b30
nguyen, thomas <thomas_nguyen@intuit.com>
2019-12-28 16:50:26 -0800

    added starting application log

--- src/main/java/src/Application.java
+++ src/main/java/src/Application.java
@@ -1,11 +1,15 @@
 package src;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
+    private static Logger logger = LoggerFactory.getLogger(Application.class);
     public static void main(String[] args) {
+        logger.info("Starting application");
         SpringApplication.run(Application.class, args);
     }
 }
